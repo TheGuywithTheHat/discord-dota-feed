15837189f592d5be8b7023f9f0cab4847b3e5359
Randy Carlson <randycarlson486@gmail.com>
2018-02-24 17:34:22 -0500

    match scanning, discord feed, match details

--- dota.js
+++ dota.js
@@ -2,6 +2,15 @@ const config = require('./config.json');
 
 const dotaAPI = require('dota2-api').create(config.steam_key);
 
+var heroes = {};
+var last = 9999999999999;
+getLatestNumber().then((latest) => { last = latest; console.log(last) });
+dotaAPI.getHeroes({ language: 'english' }).then((heroList) => {
+    for(hero of heroList.result.heroes) {
+        heroes[hero.id] = hero.localized_name;
+    }
+});
+
 function getHistories() {
     players = [];
     for(var player in config.players) {
@@ -15,18 +24,95 @@ function getHistory(id) {
 }
 
 function getLatestNumber() {
-    var max = 0;
-    getHistories().then((histories) => {
-        for(var matchList of histories) {
-            for(var match of matchList.matches) {
-                console.log(match.match_id);
+    return new Promise((resolve) => {
+        var max = 0;
+        getHistories().then((histories) => {
+            for(var matchList of histories) {
+                for(var match of matchList.result.matches) {
+                    //console.log(match.match_id + ": " + typeof match.match_id);
+                    max = Math.max(max, match.match_id);
+                }
             }
-        }
+            resolve(max);
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
+}
+
+function getLatestMatches() {
+    return new Promise((resolve) => {
+        matches = {};
+        ids = [];
+        getHistories().then((histories) => {
+            //console.log("found histories: " + histories.length);
+            for(var matchList of histories) {
+                for(var match of matchList.result.matches) {
+                    var id = match.match_id;
+                    if(id > last && !(id in matches)) {
+                        console.log("adding " + id);
+                        matches[id] = match;
+                        ids.push(id);
+                    }
+                }
+            }
+
+            if(ids.length > 0) {
+                ids.sort((a, b) => { return a - b });
+                //console.log("sorted: " + ids);
+                if(last != ids[ids.length - 1]) {
+                    console.log("New latest: " + ids[ids.length - 1]);
+                }
+                last = ids[ids.length - 1];
+
+                for(var i = 0; i < ids.length; i++) {
+                    ids[i] = matches[ids[i]];
+                }
+            }
+
+            resolve(ids);
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
+}
+
+function getMatchInfo(id) {
+    id = id + '';
+    return new Promise((resolve) => {
+        dotaAPI.getMatchDetails({ match_id: id }).then((deets) => {
+            deets = deets.result;
+            var details = {
+                players: [],
+                id: deets.match_id,
+                minutes: Math.floor(deets.duration / 60),
+                mode: deets.game_mode,
+            };
+
+            for(player of deets.players) {
+                if(player.account_id in config.players) {
+                    var p = {};
+                    p.name = config.players[player.account_id];
+                    p.hero = heroes[player.hero_id];
+
+                    p.stats = {};
+                    p.stats.gpm = player.gold_per_min;
+                    p.stats.xpm = player.xp_per_min;
+                    p.stats.kills = player.kills;
+                    p.stats.assists = player.assists;
+                    p.stats.deaths = player.deaths;
+
+                    details.players.push(p);
+                    details.won = player.player_slot > 127 != deets.radiant_win;
+                }
+            }
+
+            resolve(details);
+        });
     });
 }
 
 module.exports = {
-    getLatestNumber
+    getLatestMatches,
+    getMatchInfo,
 };
-
-getLatestNumber();
--- index.js
+++ index.js
@@ -1,18 +1,86 @@
-const config = require('config.json');
-
+const config = require('./config.json');
+const dota = require('./dota.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const thresholds = {
+    gpm: 600,
+    xpm: 675,
+    kills: 15,
+    assists: 23,
+    deaths: 30,
+};
+
 client.on('ready', () => {
     console.log('I am ready!');
+    setInterval(check, 60000);
 });
 
 client.on('message', message => {
-    if (message.author.username == 'dp') {
-        message.reply(':bad:');
-    } else if (message.content === 'ping') {
+    if (message.content === 'ping') {
         message.reply('pong');
     }
 });
+
+function sendMessage(message) {
+    //console.log('sending ' + message);
+    client.channels.get(config.discord_channel).send(message);
+}
+
+function check() {
+    //console.log("checking");
+    dota.getLatestMatches().then((matches) => {
+        //console.log("found " + matches.length + " matches");
+        for(match of matches) {
+            dota.getMatchInfo(match.match_id).then((details) => {
+                var playerList = [];
+                var description = '';
+
+                for(player of details.players) {
+                    playerList.push(`${player.name} (${player.hero})`);
+
+                    var stats = [];
+                    for(stat in player.stats) {
+                        if(player.stats[stat] >= thresholds[stat]) {
+                            stats.push(`${player.stats[stat]} ${stat}`);
+                        }
+                    }
+
+                    if(stats.length > 0 && details.mode == 22) {
+                        description += `${player.name} (${player.hero}) got ${listToText(stats)}.\n`;
+                    }
+                }       
+
+                var title = `${listToText(playerList)} just ${details.won ? 'won' : 'lost'} a ${details.minutes} minute${details.mode == 23 ? ' turbo' : ''} match${details.won ? '!' : '.'}`;
+                var url = `https://www.opendota.com/matches/${details.id}`;
+
+                var embed = new Discord.RichEmbed({
+                    title,
+                    url,
+                    description,
+                    color: details.won ? 6732650 : 16731212,
+                });
+
+                sendMessage(embed);
+            });
+        }
+    });
+}
+
+
+function listToText(list) {
+    if(list.length == 1) {
+        return `${list[0]}`;
+    } else if(list.length == 2) {
+        return `${list[0]} and ${list[1]}`;
+    } else {
+        result = '';
+        for(var i = 0; i < list.length - 1; i++) {
+            result += `${list[i]}, `;
+        }
+        result += `and ${list[list.length - 1]}`;
+        return result;
+    }
+}
 
 client.login(config.user_token);
